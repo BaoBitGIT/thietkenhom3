119528824da003fd3b20f08adb2f68a3a98a114a
BaoBitGIT <baokarahago211@gmail.com>
2026-04-08 10:46:53 +0700

    Delete validators directory

--- validators/flashcard.validator.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const reviewFlashcardSchema = z.object({
-  grade: z.enum(['again', 'hard', 'good', 'easy']),
-  responseTimeMs: z.number().int().positive().optional()
-});
