0db58839475cf3a401f69483676daee523cad107
BaoBitGIT <baokarahago211@gmail.com>
2026-04-08 10:45:28 +0700

    Delete controllers directory

--- controllers/dashboard.controller.js
+++
@@ -1,9 +0,0 @@
-import { ok } from '../utils/apiResponse.js';
-import { dashboardService } from '../services/dashboard.service.js';
-
-export const dashboardController = {
-  overview: async (req, res) => {
-    const data = await dashboardService.getOverview(req.user.id);
-    return ok(res, data, 'Dashboard overview fetched');
-  }
-};
